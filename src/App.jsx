132b89b9d2badcd6a0a92d559dca7c3782a82cc9
Wilhelm Miranda <70352471+WilAm1@users.noreply.github.com>
2023-10-27 06:19:28 +0000

    add: gh

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Wil Miranda",
+  title: "Full Stack Developer",
+  email: "wil@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "wilymiranda",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
